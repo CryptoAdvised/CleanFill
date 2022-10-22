b9b0f7c96b120a5defac196a32ecfcda2aaab527
CryptoAdvised <68133857+CryptoAdvised@users.noreply.github.com>
2022-10-21 20:18:09 -0400

    Update CleanFill.py

--- CleanFill.py
+++ CleanFill.py
@@ -1,3 +1,11 @@
+"""
+/******************************************************************************/
+/*FICHIER:      ClearFill.py                                                  */
+/*AUTEURS:      Colin Bouchard                       				          */
+/*DATE:         19/05/2020                                                    */
+/*DESCRIPTION:  Cette librarie permet de remplir les valeur NaN d'une matrie. */
+/******************************************************************************/
+"""
 from __future__ import division
 import numpy as np
 from scipy.interpolate import griddata
