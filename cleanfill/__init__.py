db9b4d5f92e13086985abea221a3bb1ccc44e6ad
CryptoAdvised <68133857+CryptoAdvised@users.noreply.github.com>
2022-10-26 09:45:23 -0400

    Add files via upload

---
+++ cleanfill/__init__.py
@@ -0,0 +1,9 @@
+from .fill import linear
+from .fill import nearest
+from .fill import slope_one
+from .fill import weighted_slope_one
+from .fill import bipolar_slope_one
+from .fill import ZeroToNaN
+from .fill import NaNToZero
+from .fill import ReplaceWithOld
+__all__ = ["fill"]
